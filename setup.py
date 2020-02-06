d2c4af9868f2eff0d4f7edb2feab6671d59163d1
lukeinthecloud <lukerbab@gmail.com>
2020-02-06 12:00:16 +0000

    feature: added in logic to deploy to heroku

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bookingly',
+    version='0.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='lbabich',
+    author_email='',
+    description='A simple booking app'
+)
