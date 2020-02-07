5010dca61dbc79e4d73a6ad454d694a39451e45d
lukeinthecloud <lukerbab@gmail.com>
2020-02-07 15:45:38 +0000

    test: added back db with different approach

--- bookingly/db.py
+++ bookingly/db.py
@@ -1,26 +1,24 @@
-# from boto.s3.connection import S3Connection
-# import os
-# import psycopg2
-#
-# DATABASE_URL = S3Connection(os.environ['DATABASE_URL'])
-#
-# print(DATABASE_URL)
-#
-# try:
-#     connection = psycopg2.connect(DATABASE_URL)
-#
-#     cursor = connection.cursor()
-#
-#     print(connection.get_dsn_parameters(), "\n")
-#
-#     cursor.execute("SELECT version();")
-#     record = cursor.fetchone()
-#     print("You are connected to - ", record, "\n")
-#
-# except (Exception, psycopg2.Error) as error:
-#     print("Error while connecting to PostgreSQL", error)
-# finally:
-#     if (connection):
-#         cursor.close()
-#         connection.close()
-#         print("PostgreSQL connection is closed")
+import os
+import psycopg2
+
+try:
+    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    print(DATABASE_URL)
+
+    connection = psycopg2.connect(DATABASE_URL)
+
+    cursor = connection.cursor()
+
+    print(connection.get_dsn_parameters(), "\n")
+
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record, "\n")
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
--- scripts/server-debug.sh
+++ scripts/server-debug.sh
@@ -2,5 +2,6 @@
 
 export FLASK_APP=bookingly
 export FLASK_ENV=development
+export DATABASE_URL='test'
 
 flask run --port 8085
