2c9ef78321a20fa90f5b1af4edcee4b331f60cbe
lukeinthecloud <lukerbab@gmail.com>
2020-02-07 15:42:29 +0000

    test: removed db

--- bookingly/db.py
+++ bookingly/db.py
@@ -1,26 +1,26 @@
-from boto.s3.connection import S3Connection
-import os
-import psycopg2
-
-DATABASE_URL = S3Connection(os.environ['DATABASE_URL'])
-
-print(DATABASE_URL)
-
-try:
-    connection = psycopg2.connect(DATABASE_URL)
-
-    cursor = connection.cursor()
-
-    print(connection.get_dsn_parameters(), "\n")
-
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record, "\n")
-
-except (Exception, psycopg2.Error) as error:
-    print("Error while connecting to PostgreSQL", error)
-finally:
-    if (connection):
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
+# from boto.s3.connection import S3Connection
+# import os
+# import psycopg2
+#
+# DATABASE_URL = S3Connection(os.environ['DATABASE_URL'])
+#
+# print(DATABASE_URL)
+#
+# try:
+#     connection = psycopg2.connect(DATABASE_URL)
+#
+#     cursor = connection.cursor()
+#
+#     print(connection.get_dsn_parameters(), "\n")
+#
+#     cursor.execute("SELECT version();")
+#     record = cursor.fetchone()
+#     print("You are connected to - ", record, "\n")
+#
+# except (Exception, psycopg2.Error) as error:
+#     print("Error while connecting to PostgreSQL", error)
+# finally:
+#     if (connection):
+#         cursor.close()
+#         connection.close()
+#         print("PostgreSQL connection is closed")
