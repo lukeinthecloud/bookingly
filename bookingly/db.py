b2dc17a62a411769c3b28c05ed88ffc3665c1f05
lukeinthecloud <lukerbab@gmail.com>
2020-02-07 16:19:54 +0000

    style: removed double quotes

--- bookingly/db.py
+++ bookingly/db.py
@@ -21,17 +21,17 @@ try:
 
     cursor = connection.cursor()
 
-    print(connection.get_dsn_parameters(), "\n")
+    print(connection.get_dsn_parameters(), '\n')
 
-    cursor.execute("SELECT version();")
+    cursor.execute('SELECT version();')
     record = cursor.fetchone()
 
-    print("You are connected to - ", record, "\n")
+    print('You are connected to - ', record, '\n')
 
 except (Exception, psycopg2.Error) as error:
-    print("Error while connecting to PostgreSQL", error)
+    print('Error while connecting to PostgreSQL', error)
 finally:
     if connection:
         cursor.close()
         connection.close()
-        print("PostgreSQL connection is closed")
+        print('PostgreSQL connection is closed')
